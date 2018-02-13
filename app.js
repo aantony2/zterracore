3f653ced1783550494e6d345cc15bf498fffd191
Agnel Antony <agnelantony2@gmail.com>
2018-02-12 22:55:50 -0500

    updated root dialog

--- app.js
+++ app.js
@@ -40,21 +40,25 @@ var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
 bot.dialog('/', function(session) {
-    var card = new builder.HeroCard(session)
-        .title("Microsoft Bot Framework")
-        .text("Your bots - wherever your users are talking.")
-        .images([
-            builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
-        ]);
-    var msg = new builder.Message(session).attachments([card]);
-    session.send(msg);
-    session.send("Hi... I'm the Microsoft Bot Framework demo bot for Slack. I can show you everything you can use our Bot Builder SDK to do on Slack.");
-    //session.beginDialog('/menu');
-});
+        var card = new builder.HeroCard(session)
+            .title("Microsoft Bot Framework")
+            .text("Your bots - wherever your users are talking.")
+            .images([
+                builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.send("Hi... I'm the Microsoft Bot Framework demo bot for Slack. I can show you everything you can use our Bot Builder SDK to do on Slack.");
+        //session.beginDialog('/menu');
+    },
+    function(session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
+    });
 
-bot.on('typing', function (message) {
+bot.on('typing', function(message) {
     var name = message.user ? message.user.name : null;
     var reply = new builder.Message()
-                .address(message.address)
-                .text("Hello %s. How can I help you today ?", name || 'there');
+        .address(message.address)
+        .text("Hello %s. How can I help you today ?", name || 'there');
 });
